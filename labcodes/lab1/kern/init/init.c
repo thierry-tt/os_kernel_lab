5a2796ed94e5214b3090f155005d5a4466b17a3e
thierry-tt <thierry@mogarret.com>
2021-06-23 12:18:27 +0800

    lab1

--- labcodes/lab1/kern/init/init.c
+++ labcodes/lab1/kern/init/init.c
@@ -1,24 +1,23 @@
+#include <clock.h>
+#include <console.h>
 #include <defs.h>
+#include <intr.h>
+#include <kdebug.h>
+#include <kmonitor.h>
+#include <picirq.h>
+#include <pmm.h>
 #include <stdio.h>
 #include <string.h>
-#include <console.h>
-#include <kdebug.h>
-#include <picirq.h>
 #include <trap.h>
-#include <clock.h>
-#include <intr.h>
-#include <pmm.h>
-#include <kmonitor.h>
 int kern_init(void) __attribute__((noreturn));
 void grade_backtrace(void);
 static void lab1_switch_test(void);
 
-int
-kern_init(void) {
+int kern_init(void) {
     extern char edata[], end[];
     memset(edata, 0, end - edata);
 
-    cons_init();                // init the console
+    cons_init();  // init the console
 
     const char *message = "(THU.CST) os is loading ...";
     cprintf("%s\n\n", message);
@@ -27,20 +26,21 @@ kern_init(void) {
 
     grade_backtrace();
 
-    pmm_init();                 // init physical memory management
+    pmm_init();  // init physical memory management
 
-    pic_init();                 // init interrupt controller
-    idt_init();                 // init interrupt descriptor table
+    pic_init();  // init interrupt controller
+    idt_init();  // init interrupt descriptor table
 
-    clock_init();               // init clock interrupt
-    intr_enable();              // enable irq interrupt
+    clock_init();   // init clock interrupt
+    intr_enable();  // enable irq interrupt
 
     //LAB1: CAHLLENGE 1 If you try to do it, uncomment lab1_switch_test()
     // user/kernel mode switch test
-    //lab1_switch_test();
+    lab1_switch_test();
 
     /* do nothing */
-    while (1);
+    while (1)
+        ;
 }
 
 void __attribute__((noinline))
@@ -58,8 +58,7 @@ grade_backtrace0(int arg0, int arg1, int arg2) {
     grade_backtrace1(arg0, arg2);
 }
 
-void
-grade_backtrace(void) {
+void grade_backtrace(void) {
     grade_backtrace0(0, (int)kern_init, 0xffff0000);
 }
 
@@ -67,28 +66,39 @@ static void
 lab1_print_cur_status(void) {
     static int round = 0;
     uint16_t reg1, reg2, reg3, reg4;
-    asm volatile (
-            "mov %%cs, %0;"
-            "mov %%ds, %1;"
-            "mov %%es, %2;"
-            "mov %%ss, %3;"
-            : "=m"(reg1), "=m"(reg2), "=m"(reg3), "=m"(reg4));
+    asm volatile(
+        "mov %%cs, %0;"
+        "mov %%ds, %1;"
+        "mov %%es, %2;"
+        "mov %%ss, %3;"
+        : "=m"(reg1), "=m"(reg2), "=m"(reg3), "=m"(reg4));
     cprintf("%d: @ring %d\n", round, reg1 & 3);
     cprintf("%d:  cs = %x\n", round, reg1);
     cprintf("%d:  ds = %x\n", round, reg2);
     cprintf("%d:  es = %x\n", round, reg3);
     cprintf("%d:  ss = %x\n", round, reg4);
-    round ++;
+    round++;
 }
 
 static void
 lab1_switch_to_user(void) {
     //LAB1 CHALLENGE 1 : TODO
+    asm volatile(
+        "sub $0x8, %%esp \n"
+        "int %0 \n"
+        "movl %%ebp, %%esp"
+        :
+        : "i"(T_SWITCH_TOU));
 }
 
 static void
 lab1_switch_to_kernel(void) {
     //LAB1 CHALLENGE 1 :  TODO
+    asm volatile(
+        "int %0 \n"
+        "movl %%ebp, %%esp \n"
+        :
+        : "i"(T_SWITCH_TOK));
 }
 
 static void
@@ -101,4 +111,3 @@ lab1_switch_test(void) {
     lab1_switch_to_kernel();
     lab1_print_cur_status();
 }
-
